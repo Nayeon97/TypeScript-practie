f0a3f55b20c84e12ddbb5b4c118456c36a4a4b40
Nayeon <89979344+Nayeon97@users.noreply.github.com>
2022-07-21 21:36:44 +0900

    Update index.ts

--- pratice/1_todo/src/index.ts
+++ pratice/1_todo/src/index.ts
@@ -51,7 +51,7 @@ function showCompleted(): object[] {
   return todoItems.filter(item => item.done);
 }
 
-// TODO: 아래 함수의 내용을 채워보세요. 아래 함수는 `addTodo()` 함수를 이용하여 2개의 새 할 일을 추가하는 함수입니다.
+
 function addTwoTodoItems() : void{
   const item1 = {
     id: 4,

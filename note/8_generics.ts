1979bc276e38b7f442745150d3ed379830b164f1
Naayeon <naaay097@gmail.com>
2022-07-27 23:44:15 +0900

    📖 제네릭 keyof 선언 방법

--- note/8_generics.ts
+++ note/8_generics.ts
@@ -70,3 +70,48 @@ interface Dropdown<T>{
 }
 
 const obj: Dropdown<string> = { value: 'abc', selected: false}
+
+// 제네릭 타입 제한 
+function logTextLength<T>(text: T[]): T[] {
+    //console.log(text.length); // Property 'length' does not exist on type 'T'  -> logTextLength 에 어떤 타입이  들어올지 알수없다! 
+    //  logTextLength<T>(text: T): T 대신 
+    // logTextLength<T>(text: T[]): T[]  으로 하면 에러 해결!  -> logTextLength<string>(['hi', 'abc']);   으로 수정! 
+
+    return text;
+}
+
+// logTextLength('hi'); 
+logTextLength<string>(['hi', 'abc']); 
+
+// 제네릭 타입 제한  2 - 정의된 타입 이용하기.
+interface LengthType {
+    length: number;
+}
+
+// 상위에 정의된 타입들을 확장해서 쓸때 extends! 
+function logTextLength2<T extends LengthType>(text: T): T{
+text.length;
+return text;
+}
+
+logTextLength2('a');
+logTextLength2({length: 2000});
+// logTextLength2({apple: 2000});
+// logTextLength2(10); // error -> number 에는 length 가 제공되지 않음! 
+
+
+// 제네릭 타입 제한 3 - keyof 이용! 
+interface ShoppingItem {
+    name: string;
+    price: number;
+    stock: number;
+}
+
+function getShoppingItemOption<T extends keyof ShoppingItem>(itemOption: T) {
+return itemOption;
+}
+
+// getShoppingItemOption(10);
+// getShoppingItemOption<string>('a');
+
+getShoppingItemOption('name');
